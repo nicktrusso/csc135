4454706eb7b9f96cb5e0a5d4b588575605ff50b1
Nick Trusso <cobbtuned@comcast.net>
2015-11-07 20:31:41 -0800

    Update scheme.rkt

--- assignment2/scheme.rkt
+++ assignment2/scheme.rkt
@@ -1,3 +1,4 @@
+;****************** PROBLEM A *****************************
 ;takes two integers, and returns a 4-digit integer constructed of the leftmost 2 digits of the
 ;first input, and the rightmost 2 digits of the second input.       
 (define(buildLR i1 i2)
@@ -6,8 +7,7 @@
        ((not(isTwoDigit i2)) -1)
        (else(+ (* 100 (firstDigits i1))(lastDigits i2)))))
        
-
-;Helper function
+;Helper functions
 (define(isTwoDigit n)
   (or (> n 9)(< n -9))) 
 
@@ -18,6 +18,7 @@
 
 (define(lastDigits n)(modulo (abs n) 100))
 
+;****************** PROBLEM B **************************** 
 ;listMins takes two equal-length lists of numbers, and returns a single list 
 ;consisting of the smaller ofthe two lists, position by position.
 (define(listMins l1 l2)
@@ -25,7 +26,10 @@
        ((>(car l1)(car l2))(cons(car l2)(listMins(cdr l1)(cdr l2))))
        (else(cons(car l1)(listMins(cdr l1)(cdr l2))))))
        
-       
+;****************** PROBLEM C *****************************
+
+ 
+;****************** PROBLEM D *****************************       
 ;takes two Boolean functions F and G, and a list L. It returns the number 1 if
 ;function F is the "winner", and the number 2 if function G is the "winner", 
 ;and the number 0 if the two functions are tied. The winning function is the one 
@@ -35,23 +39,24 @@
        ((>(boolCount F L)(boolCount G L)) 1)
        ((>(boolCount G L)(boolCount F L)) 2)
        (else 0)))
-  
 
+;helper functions
 (define(boolCount F L)
   (cond((null? L) 0)
        ((F (car L))(+ 1(boolCount F (cdr L))))
        (else (boolCount F (cdr L)))))
                        
-;Helper functions for testing
-;isNeg
+;testing
 (define(isNeg n)(< n 0))
-;isEven
 (define(isEven n)(= 0 (modulo n 2)))
 
-
+;****************** PROBLEM E *****************************
 ;takes a list of integers, possibly including nested lists, and returns the total number
 ;of integers in the entire nested list.
 (define(getNestedCount l)
   (cond((null? l) 0)
        ((list? (car l))(+ (getNestedCount(car l))(getNestedCount(cdr l))))
        (else(+ 1(getNestedCount(cdr l))))))
+       
+;****************** PROBLEM F *****************************
+
